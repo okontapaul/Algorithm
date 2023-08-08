813baea22fb6fe32cd3aeaed610c2f7bff5c4395
okontapaul <pokonta@yahoo.com>
2023-08-08 12:33:07 +0100

    Algorithm

---
+++ index.ts
@@ -0,0 +1,27 @@
+// vaiable declariation 
+// var firstName: string;
+// var lastName: string;
+// var married: boolean | string;
+// var phonenumber: number;
+// var dissabilities: null;
+// var accountBalance: undefined | number;
+
+
+
+
+// // variable assignment
+// // firstName = "Paul"
+// lastName = "Okonta"
+// married = true
+// phoneNumber = 7080776398
+// dissabilities = null
+// accountBalance = 1000 
+
+
+// variable init
+var firstName = "Paul"
+var lastName = "Okonta"
+var married = "true"
+var phoneNumber = 7080776398
+var dissabilities = null
+var accountBalance = 1000
